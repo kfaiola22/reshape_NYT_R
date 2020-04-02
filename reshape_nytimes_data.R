60c300d59ca978f0b39eeb6f8468acbb0156e166
kfaiola22 <62571288+kfaiola22@users.noreply.github.com>
2020-04-02 12:15:25 -0400

    Add files via upload

---
+++ reshape_nytimes_data.R
@@ -0,0 +1,71 @@
+
+# read in the data
+dat <- read.csv("us-counties.csv",as.is=TRUE)
+
+# main idea: create a matrix that has one row 
+# for each county, one column for each dat
+
+
+county_map <- ggplot2::map_data("county")
+
+state_county <- unique(paste(county_map$region, county_map$subregion, sep = "@" ))
+ncounties <- length(state_county)
+
+st_co_list <- strsplit( state_county, "@", fixed = TRUE )
+
+state <- rep(NA,ncounties)
+county <- rep(NA,ncounties)
+for(j in 1:ncounties){
+    state[j] <- st_co_list[[j]][1]
+    county[j] <- st_co_list[[j]][2]
+}
+
+st_co <- data.frame( state = state, county = county )
+
+first_date <- as.Date( dat$date[1], format = "%Y-%m-%d" )
+last_date <- as.Date( dat$date[ length(dat$date) ], format = "%Y-%m-%d")
+all_dates_num <- first_date:last_date
+ndays <- length(all_dates_num)
+all_dates <- first_date + (0:(ndays-1))
+
+cases <- matrix(0, ncounties, ndays )
+deaths <- matrix(0, ncounties, ndays )
+
+# loop over all rows in nytimes dataset
+for (row in 1:nrow(dat)) {
+  # isolate the state, county, case count and death count from NYT dataset, dat
+  st <- dat$state[row]
+  # error: st only contains one state... 
+  co <- dat$county[row]
+  case_ct <- dat$cases[row]
+  death_ct <- dat$deaths[row]
+  # vector of what a reshaped row would contain 
+  st_co_cases_deaths <- c(st,co,case_ct,death_ct)
+  
+  # want to insert case and death counts in consecutive columns of same row - iterate thru days
+  day <- dat$date[row]
+  for (i in (1:day)) {
+    cases[i,] <- st_co_cases_deaths[3]
+    deaths[i,] <- st_co_cases_deaths[4]
+  }
+
+
+}
+# for each row, figure out which row and column
+# we should put the data in 'cases' and 'deaths'
+
+# put the data in 'cases' and 'deaths'
+
+# modify the matrices 
+# cases[,] <- case_ct; cases
+# deaths[,] <- death_ct; deaths
+
+# do some spot checking for accuracy
+
+
+
+# save as csv or .RData file
+# write.csv
+
+save( st_co, all_dates, cases, deaths, file = "reshaped_nytimes_data.RData")
+
