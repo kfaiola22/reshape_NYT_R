178e04859e7a4b6dd969f6e1858faad87aa33195
joeguinness <joeguinness@gmail.com>
2020-04-07 15:29:32 -0400

    updates I forgot to push yesterday

--- plot_deaths.R
+++ plot_deaths.R
@@ -1,29 +1,23 @@
 
+args <- commandArgs(trailingOnly = TRUE)
+region <- args[1]
+
 # plot the nytimes data
-source("reshape_nytimes_data.R")
+#source("reshape_nytimes_data.R")
 
 # read in the matrix of cases and deaths
 load("reshaped_nytimes_data.RData")
 
-county_map <- ggplot2::map_data("county")
-state_map <- ggplot2::map_data("state")
+# this we'll have to read in from a text file
+fname <- paste0("regions/",region,".txt")
+states <- read.table(fname, as.is=TRUE)[,1]
+
+
 library("ggplot2")
+county_map <- map_data("county", region = states)
+state_map <- map_data("state", region = states)
 
-county_map$cases <- NA
-county_map$deaths <- NA
-for( j in 1:nrow(st_co) ){
-    i1 <- county_map$region == st_co$state[j]
-    i2 <- county_map$subregion == st_co$county[j]
-    inds <- i1 & i2
-    county_map$cases[inds] <- cases[j,ncol(cases)]
-    county_map$deaths[inds] <- deaths[j,ncol(deaths)]
-}
     
-    
-# play around with colors, projections,
-# transformations (log, etc.)
-# write a loop to make a bunch of plots, one for each day
-
 for(j in (ncol(cases)-3):ncol(cases)){
     county_map$cases <- NA
     county_map$deaths <- NA
@@ -53,7 +47,7 @@ for(j in (ncol(cases)-3):ncol(cases)){
         fill = "white",
         alpha = 0
     )
-    fname <- paste0( "testplot", j, ".png" )
+    fname <- paste0( region, j, ".png" )
     png(fname,width=800,height=600)
     print(p1 + p2 + p5 + p6 + p3 + p4 + p7 + p8)
     dev.off()
